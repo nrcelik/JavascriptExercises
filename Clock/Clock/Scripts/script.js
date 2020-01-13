dd7a5ba51c3a299dea1f4f7a49c43d8469024aa7
nrcelik <59237286+nrcelik@users.noreply.github.com>
2020-01-13 22:54:43 +0000

    Update script.js

--- Clock/Clock/Scripts/script.js
+++ Clock/Clock/Scripts/script.js
@@ -1,3 +1,11 @@
 ﻿const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute")
 const SECONDHAND = document.querySelector("#second");
+
+let hrPosition = 20;
+let mnPosition = 130;
+let scPosition = 267;
+
+HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+MINUTEHAND.style.transform = "rotate(" + mnPosition + "deg)";
+SECONDHAND.style.transform = "rotate(" + scPosition + "deg)";
