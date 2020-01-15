c98104e65b7734384f73396305e8a35451e53cee
nrcelik <59237286+nrcelik@users.noreply.github.com>
2020-01-15 09:55:28 +0000

    A dynamic Clock is created and it works correctly.

--- Clock/Clock/Scripts/script.js
+++ Clock/Clock/Scripts/script.js
@@ -1,11 +1,30 @@
 ﻿const HOURHAND = document.querySelector("#hour");
-const MINUTEHAND = document.querySelector("#minute")
+const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
-let hrPosition = 20;
-let mnPosition = 130;
-let scPosition = 267;
+var date = new Date();
+console.log(date);
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+console.log("Hour: " + hr + " Minute: " + min + " Second: " + sec);
 
-HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
-MINUTEHAND.style.transform = "rotate(" + mnPosition + "deg)";
-SECONDHAND.style.transform = "rotate(" + scPosition + "deg)";
+let hrPosition = (hr * 360 / 12) + (min * (360 / 60) / 12);
+let minPosition = (min * 360 / 60) + (sec * (360 / 60) / 60);
+let secPosition = sec * 360 / 60;
+
+function runTheClock() {
+
+    hrPosition = hrPosition + (3 / 360);
+    minPosition = minPosition + (6 / 60);
+    secPosition = secPosition + 6;
+
+    HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+    MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+    SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+
+}
+
+var interval = setInterval(runTheClock, 1000);
+
+
